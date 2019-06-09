99bc9cd1c9f9ba369db0ce0bef9c7679e14f3662
Zacharie Day <day@gwu.edu>
2019-06-09 19:32:03 -0400

    Lol

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use quicksilver::geom::{Vector, Rectangle};
+use quicksilver::input::{Keyboard, Key};
+
+struct Player<'a> {
+    rect: Rectangle,
+    velocity: Vector,
+    keyboard: &'a Keyboard,
+}
+
+
 fn main() {
     println!("Hello, world!");
 }
